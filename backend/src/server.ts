dcd45ca4bcdd58e3b67524b3b73cadd34563097e
Leonardo Santos <leonardosanttos0077@gmail.com>
2020-10-13 22:43:18 -0300

    Adicionando imagens

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,37 +1,11 @@
 import express from 'express'
-import { getRepository } from 'typeorm' 
-
 import './database/connection';
-import Orphanage from './models/Orphanage';
+
+import routes from './routes';
+
 const app = express()
+
 app.use(express.json())
-
-app.post('/orphanages', async (requests, response) => { 
-    const {
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-    } = requests.body;
-
-    const orphanagesRepository = getRepository(Orphanage)
-    const orphanage = orphanagesRepository.create({
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-    });
-
-    await orphanagesRepository.save(orphanage)
-
-    return response.status(201).json({'status':'success'})
-
-});
+app.use(routes)
 
 app.listen(3333);
